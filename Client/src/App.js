52e3ce4c830c681cd02e15e36fb92b11216f3105
Imene Louni <li_louni@esi.dz>
2023-04-27 18:49:00 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,25 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 
+// import HomePage from "./pages/Home";
 import Doctorant from "./pages/Doctorant";
-import popup from "./pages/popup";
+// import Inscription from "./pages/Inscription";
+// import popup from "./pages/popup";
 import Acceuil from './pages/Acceuil';
 import Connexion from "./pages/Connexion";
-import Update from "./pages/Update";
-import PVs from "./pages/PVs";
-import Users from "./pages/Users";
-import Profile from "./pages/Profile";
-import Editprofile from "./pages/Editprofile";
-import Dashboard from "./pages/Dashboard";
-import Statistics from "./pages/Statistics";
-import Seminaire from "./pages/Seminaire";
-import InscriptionStep1 from "./pages/InscriptionStep1";
-import InscriptionStep2 from "./pages/InscriptionStep2";
-import InscriptionStep3 from "./pages/InscriptionStep3";
-import ConfirmEndacrant from "./pages/ConfirmEndacrant";
-import SeminaireLirePlus from "./pages/SeminaireLirePlus";
+// import Update from "./pages/Update";
+// import PVs from "./pages/PVs";
+// import Users from "./pages/Users";
+// import Profile from "./pages/Profile";
+// import Editprofile from "./pages/Editprofile";
+// import Dashboard from "./pages/Dashboard";
+// import Statistics from "./pages/Statistics";
+// import Seminaire from "./pages/Seminaire";
+// import InscriptionStep1 from "./pages/InscriptionStep1";
+// import InscriptionStep2 from "./pages/InscriptionStep2";
+// import InscriptionStep3 from "./pages/InscriptionStep3";
+// import ConfirmEndacrant from "./pages/ConfirmEndacrant";
+// import SeminaireLirePlus from "./pages/SeminaireLirePlus";
 
 
 
@@ -33,6 +35,9 @@ export default function App() {
             <Connexion />
           </Route>
           <Route path="/Doctorant" exact>
+            <Doctorant/>
+          </Route>
+          {/* <Route path="/Doctorant" exact>
             <Doctorant />
           </Route>
           <Route path="/Dashboard" exact>
@@ -72,7 +77,7 @@ export default function App() {
           <Route path="/Seminaire" exact>
             <Seminaire />
           </Route>
-          <Route path="/SeminaireLirePlus/:title" exact component={SeminaireLirePlus} />
+          <Route path="/SeminaireLirePlus/:title" exact component={SeminaireLirePlus} /> */}
         </Switch>
       </Router>
     </div>
